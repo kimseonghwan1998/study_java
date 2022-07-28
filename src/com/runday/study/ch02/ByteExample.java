904b92db1c385b2f2fd167638c7ac469d52bef19
ksh <fjscu112@naver.com>
2022-07-28 17:29:49 +0900

    333

---
+++ src/com/runday/study/ch02/ByteExample.java
@@ -0,0 +1,22 @@
+package com.runday.study.ch02;
+
+public class ByteExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// byte 범위는 -127~127까지
+		
+		byte var1= -128;
+		byte var2= -30;
+		byte var3= 0;
+		byte var4= 30;
+		byte var5= 127;
+		
+		System.out.println(var1);
+		System.out.println(var2);
+		System.out.println(var3);
+		System.out.println(var4);
+		System.out.println(var5);
+	}
+
+}
---
+++ src/com/runday/study/ch02/Hello.java
@@ -0,0 +1,33 @@
+package com.runday.study.ch02;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		데이터타입 변수명 = 값;
+//		데이터타입 변수명
+		
+		String cus = "고객센터";
+		String dis = "주행거리";
+		String cos = "코스추천";
+		String crew = "러닝크루";
+		String bod = "게시판";
+		String user = "김승환님";
+	
+		double km = 5.31;
+		double kcal = 345;
+		double heit = 5;
+		int cadence = 183;
+		
+		System.out.println("cus 변수에 들어가 있는 값은: " + cus);
+		System.out.println("dis 변수에 들어가 있는 값은: " + dis);
+		System.out.println("cos 변수에 들어가 있는 값은: " + cos);
+		System.out.println("crew 변수에 들어가 있는 값은: " + crew);
+		System.out.println("bod 변수에 들어가 있는 값은: " + bod);
+		System.out.println("user 변수에 들어가 있는 값은: " + user);
+		
+		System.out.println("km 변수에 들어가 있는 값은: " + km);
+		System.out.println("kcal 변수에 들어가 있는 값은: " + kcal);
+		System.out.println("heit 변수에 들어가 있는 값은" + heit);
+	}
+}
