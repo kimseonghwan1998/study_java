bb7c6cb023239dd9cdcdb3631c938ec49b559e94
ksh <fjscu112@naver.com>
2022-08-05 17:35:38 +0900

    3123

---
+++ src/com/runday/study/ch03/AssignmentOperatorExample.java
@@ -0,0 +1,32 @@
+package com.runday.study.ch03;
+
+public class AssignmentOperatorExample {
+
+	public static void main(String[] args) {
+		// +=
+		
+		int result = 0;
+		int result1 = 10;
+		int result2 = 5;
+		
+		result += 10;
+//		result = result + 10;
+		
+		System.out.println("result: " + result);
+
+// -=
+		result1 -= 20;
+//		result1 = result - 20 ;
+		System.out.println("result1: " + result1);
+		
+// *=
+		result2 *= 30;
+//		result2 = result * 30;
+		System.out.println("result2:" + result2);
+		
+		
+		
+
+	}
+
+}
