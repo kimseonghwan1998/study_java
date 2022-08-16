1699ac9eb135415f1708f13e29b6795c2ffc0fa7
ksh <fjscu112@naver.com>
2022-08-16 17:29:58 +0900

    12323

---
+++ src/com/runday/study/ch03/randomsum2.java
@@ -0,0 +1,31 @@
+package com.runday.study.ch03;
+
+public class randomsum2 {
+
+	public static void main(String[] args) {
+// 1~100 사이의 랜덤 정수 2개를 발생시켜
+// 두 수 사이의 합을 구하라.
+		double num1 = 1.0;
+		double num2 = 100.0;
+		
+		int sum = 0, min = 0, max = 0;
+		
+		int random1 = (int) ((Math.random()*(num2 - num1)) + num1);
+		int random2 = (int) ((Math.random()*(num2 - num1)) + num1);
+		
+		if (random1 < random2) {
+			max = random2;
+			min = random1;
+		}else {
+			max = random1;
+			min = random2;
+		}
+		System.out.println("min :" + min + "," + "max :" + max );
+		
+		for(int i = min; i <= max; i++) {
+			sum +=i;
+			System.out.println(i + ":" + sum);
+		}
+		System.out.println("total:" + sum);
+	}
+}
---
+++ src/com/runday/study/ch04/ForExample.java
@@ -0,0 +1,31 @@
+package com.runday.study.ch04;
+
+public class ForExample {
+
+	public static void main(String[] args) {
+		
+//1
+	for(int i=1; i<=5; i++) {
+		for(int j=1; j<=10; j++) {
+			System.out.print("*");
+		}
+		System.out.println();
+	}
+		System.out.println("---------------------");
+//2	
+		for (int i = 1; i <= 5; i++) {
+			for (int j = 1; j <= 5; j++) {
+
+				if ((i > 1 && i < 5) && (j == 2 || j == 3 | j == 4)) {
+					System.out.print(" ");
+				} else System.out.print("*"); 				
+			}
+			System.out.print("\n");
+		}
+		
+//3 
+		System.out.println("---------------------");
+		
+		
+	}
+}
